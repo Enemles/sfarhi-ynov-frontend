cdd6e8dbb64bc93a3c76fa8136825854c57ce243
Selmene <enemles.ihraf@gmail.com>
2024-04-30 09:42:17 +0200

    karma

---
+++ karma.conf.js
@@ -0,0 +1,39 @@
+const { join } = require('path');
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-coverage')
+    ],
+    client: {
+      clearContext: false
+    },
+    coverageReporter: {
+      dir: join(__dirname, './coverage'),
+      reporters: [
+        { type: 'lcov', subdir: '.' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    },
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
