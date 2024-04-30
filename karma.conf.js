59187f104e4780c594592499208357c07c2c88ae
Selmene <enemles.ihraf@gmail.com>
2024-04-30 11:10:51 +0200

    update karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,5 @@
-const { join } = require('path');
-
-process.env.CHROME_BIN = require('puppeteer').executablePath()
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
@@ -10,32 +9,31 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-coverage')
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: join(__dirname, './coverage'),
+      dir: require('path').join(__dirname, './coverage/angular-17-crud'),
+      subdir: '.',
       reporters: [
-        { type: 'lcov', subdir: '.' },
+        { type: 'html' },
         { type: 'text-summary' }
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu', '--disable-translate', '--disable-extensions', '--remote-debugging-port=9222']
-      }
-    },
-    singleRun: true,
+    browsers: ['ChromeHeadless'],
     restartOnFileChange: true
   });
 };
